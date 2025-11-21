aa635f20edcaad769355f475a2b983273af974b3
DIO0550 <47467473+DIO0550@users.noreply.github.com>
2025-11-21 12:48:44 +0000

    ✨ [New Feature]: table/index.tsにtr要素のエクスポートを追加
    - TrElement と TrAttributes を公開インターフェースに追加
    - テーブル要素（td, th, tr）すべてが利用可能に
    
    Refs #149

--- src/converter/elements/table/index.ts
+++ src/converter/elements/table/index.ts
@@ -2,3 +2,5 @@ export { TdElement } from "./td";
 export type { TdAttributes } from "./td";
 export { ThElement } from "./th";
 export type { ThAttributes } from "./th";
+export { TrElement } from "./tr";
+export type { TrAttributes } from "./tr";

d9bfea2b50734d825579673249f7beac8de4435c
DIO0550 <47467473+DIO0550@users.noreply.github.com>
2025-11-30 12:15:04 +0000

    🚨 [Tests]: SVG基本図形の統合テスト追加
    SVG基本図形（circle、rect、line、ellipse）のFigma変換統合テストを追加：
    - 各要素の基本的な変換動作の検証
    - 属性適用の統合検証
    - エクスポート設定の確認
    
    Refs #131

---
+++ src/converter/elements/svg/index.ts
@@ -0,0 +1,26 @@
+// SVG共通属性
+export {
+  SvgAttributes,
+  type SvgPresentationAttributes,
+  type SvgBaseAttributes,
+} from "./svg-attributes";
+
+// ユーティリティ
+export { SvgPaintUtils } from "./utils/svg-paint-utils";
+export {
+  SvgCoordinateUtils,
+  type BoundingBox,
+} from "./utils/svg-coordinate-utils";
+
+// 基本図形要素
+export { CircleElement } from "./circle";
+export type { CircleAttributes } from "./circle";
+
+export { RectElement } from "./rect";
+export type { RectAttributes } from "./rect";
+
+export { LineElement } from "./line";
+export type { LineAttributes } from "./line";
+
+export { EllipseElement } from "./ellipse";
+export type { EllipseAttributes } from "./ellipse";
